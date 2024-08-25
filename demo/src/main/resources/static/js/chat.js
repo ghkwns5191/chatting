93b913f6b2c265011ce0b64aa7f5a94be4172d0a
ghkwns5191 <ghkwns5191@gmail.com>
2024-08-25 18:19:40 +0900

    접속 세션 정의 수정

--- demo/src/main/resources/static/js/chat.js
+++ demo/src/main/resources/static/js/chat.js
@@ -37,12 +37,12 @@ $(document).ready(function(){
     
 
     function send(){
-
+		var sendSessionVal = document.getElementById("sessionVal").value
         let msg = document.getElementById("msg");
 		username = document.getElementById('nameon').value;
 		if (msg.value != null && msg.value != "") {
 	        console.log(username + ":" + msg.value);
-	        websocket.send(username + ":" + msg.value);
+	        websocket.send(username + ":" + msg.value + ".." + sendSessionVal);
         }
         msg.value = '';
     }
@@ -56,10 +56,14 @@ $(document).ready(function(){
     //채팅창에 들어왔을 때
     function onOpen(evt) {
         /*var str = username + ": 님이 입장하셨습니다.";*/
-    }g
+    }
 
     function onMessage(msg) {
         var data = msg.data;
+        
+        var sessionVal = document.getElementById("sessionVal").value;
+        console.log(document.getElementById("sessionVal").value);
+        console.log(msg);
         var sessionId = null;
         //데이터를 보낸 사람
         var message = null;
@@ -69,12 +73,12 @@ $(document).ready(function(){
             console.log('arr[' + i + ']: ' + arr[i]);
         }
 
-        var cur_session = username;
+        var cur_session = sessionVal;
 
         //현재 세션에 로그인 한 사람
         console.log("cur_session : " + cur_session);
-        sessionId = arr[0];
-        message = arr[1];
+        sessionId = arr[1].substring(arr[1].lastIndexOf("..") + 2, arr[1].length);
+        message = arr[1].substring(0, arr[1].lastIndexOf(".."));
 
         console.log("sessionID : " + sessionId);
         console.log("cur_session : " + cur_session);
@@ -83,16 +87,16 @@ $(document).ready(function(){
         if(sessionId == cur_session){
             var str = "<div class='col-6'>";
             str += "<div class='alert alert-secondary' style='text-align: right'>";
-            str += "<b>" + sessionId + " : " + message + "</b>";
+            str += "<b>" + arr[0] + " : " + message + "</b>";
             str += "</div></div>";
             $("#msgArea").append(str);
 
-            $("#txtarea").text(sessionId + " : " + message);
+            $("#txtarea").text(arr[0] + " : " + message);
         }
         else{
             var str = "<div class='col-6'>";
             str += "<div class='alert alert-warning' style='text-align: left'>";
-            str += "<b>" + sessionId + " : " + message + "</b>";
+            str += "<b>" + arr[0] + " : " + message + "</b>";
             str += "</div></div>";
             $("#msgArea").append(str);
         }
