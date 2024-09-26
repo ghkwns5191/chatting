c1e7533a3a93d093d514563bd428c4c1cb28e28d
ghkwns5191 <ghkwns5191@gmail.com>
2024-09-26 18:08:07 +0900

    회원가입 로그인 기능 security 적용 없이 구현 & 쿼리 로그 적용

--- demo/src/main/java/com/example/demo/chatting/ChattingController.java
+++ demo/src/main/java/com/example/demo/chatting/ChattingController.java
@@ -3,17 +3,28 @@ package com.example.demo.chatting;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ChattingController {
+	
+	private static final Logger log = LoggerFactory.getLogger(ChattingController.class);
+	
 	
 	@Autowired
 	private ChattingService chattingService;
@@ -25,7 +36,7 @@ public class ChattingController {
 		model.addAttribute("sessionVal", sessionVal);
 		
 		Map<String, Object> datas = new HashMap<>();
-//		datas = this.chattingService.getDialogs();
+		datas = this.chattingService.getDialogs();
 		
 		return "ui/login";
 	}
@@ -50,7 +61,9 @@ public class ChattingController {
 		String sessionVal = session.getId();
 		model.addAttribute("sessionVal", sessionVal);
 		
-		Map<String, Object> datas = new HashMap<>();
+//		this.chattingService.login(request, response);
+//		
+//		Map<String, Object> datas = new HashMap<>();
 //		datas = this.chattingService.getDialogs();
 		
 		return "ui/login";
@@ -60,12 +73,67 @@ public class ChattingController {
 	@RequestMapping("/room")
 	public String room(HttpServletRequest request, HttpServletResponse response, Model model) {
 		HttpSession session = request.getSession();
+		
 		String sessionVal = session.getId();
 		model.addAttribute("sessionVal", sessionVal);
 		
+		log.info(sessionVal);
 		Map<String, Object> datas = new HashMap<>();
 //		datas = this.chattingService.getDialogs();
 		
 		return "ui/index";
+	}
+	
+	
+	@RequestMapping(value = "/login", method=RequestMethod.POST)
+	public ResponseEntity<String> login(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String, Object> loginInfo) {
+		boolean result = false;
+		log.info(loginInfo.toString());
+		Map<String, Object> loginResult = this.chattingService.login(request, response, loginInfo);
+		log.info(loginResult.toString());
+		if ((boolean) loginResult.get("loginFlag")) {
+			result = true;
+			
+			
+			HttpSession newSession = (HttpSession) loginResult.get("session");
+			newSession.setAttribute("username", Util.mapToString(loginResult, "username"));
+			log.info(newSession.toString());
+			Cookie cookie = new Cookie("username", Util.mapToString(loginResult, "username"));
+			response.addCookie(cookie);  // 쿠키를 클라이언트에 추가
+			log.info(cookie.toString());
+		} else {
+			result = false;
+		}
+		String res = "";
+		
+		if (result) {
+			res = "success";
+		} else {
+			res = "fail";
+		}
+		
+		
+		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
+	
+	
+	@RequestMapping(value = "/join", method=RequestMethod.POST)
+	public ResponseEntity<String> join(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String, Object> joinInfo) {
+		boolean result = false;
+		
+		Map<String, Object> joinResult = this.chattingService.join(request, response, joinInfo);
+		if ((boolean) joinResult.get("joinFlag")) {
+			result = true;
+		} else {
+			result = false;
+		}
+		String res = "";
+		if (result) {
+			res = "success";
+		} else {
+			res = "fail";
+		}
+		
+		return new ResponseEntity<>(res, HttpStatus.OK);
 	}
 }
--- demo/src/main/java/com/example/demo/chatting/ChattingService.java
+++ demo/src/main/java/com/example/demo/chatting/ChattingService.java
@@ -1,22 +1,124 @@
 package com.example.demo.chatting;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Transactional
 @Service
 public class ChattingService {
 	
+	private static final Logger log = LoggerFactory.getLogger(ChattingService.class);
+	
 	@Autowired
 	SqlSession sql;
+	
+	
 	
 	private static final String NAMESPACE = "chatting.";
 	
 	public Map<String, Object> getDialogs() {
 		return sql.selectOne(NAMESPACE + "getDialogs");
+	}
+	
+	
+	public Map<String, Object> login(HttpServletRequest request, HttpServletResponse response, Map<String, Object> loginInfo) {
+		Map<String, Object> result = new HashMap<>();
+		boolean loginFlag = false;
+		String username = "";
+		HttpSession session = request.getSession();
+		SHA256 sha = new SHA256();
+		
+		Map<String, Object> infoById = Util.mapValidate(sql.selectOne(NAMESPACE + "getInfoById", loginInfo));
+		log.info((String)infoById.get("USERNAME"));
+		log.info(infoById.toString());
+		if ((String)infoById.get("USERNAME") == null) {
+			loginFlag = false;
+
+		} else {
+			username = Util.mapToString(infoById, "USERNAME");
+			String savedPassword = Util.mapToString(infoById, "PASSWORD");
+			String inputPassword = Util.mapToString(loginInfo, "password");
+			
+			try {
+				inputPassword = sha.encrypt(inputPassword);
+				
+				log.info(savedPassword);
+				log.info(inputPassword);
+				
+				loginFlag = sha.validatePassword(savedPassword, inputPassword);
+				
+				if (loginFlag) {
+					request.getSession().invalidate();  // 기존 세션 무효화
+					session = request.getSession(true);
+				} else {
+					
+				}
+			} catch(Exception e) {
+				
+			}
+			
+			
+		}
+		log.info(username);
+		log.info(String.valueOf(loginFlag));
+		log.info(session.toString());
+		result.put("username", username);
+		result.put("loginFlag", loginFlag);
+		result.put("session", session);
+		
+		
+		return result;
+	}
+	
+	
+	
+	public Map<String, Object> join(HttpServletRequest request, HttpServletResponse response, Map<String, Object> joinInfo) {
+		
+		Map<String, Object> result = new HashMap<>();
+		boolean joinFlag = false;
+		String password = Util.mapToString(joinInfo, "password");
+		SHA256 sha = new SHA256();
+		
+		
+		try {
+			password = sha.encrypt(password);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		joinInfo.put("password", password);
+		
+		List<Map<String, Object>> usernameData = sql.selectList(NAMESPACE + "checkUsername", joinInfo);
+		
+		if (usernameData.size() > 0) {
+			joinFlag = false;
+		} else if (usernameData.size() == 0) {
+			joinFlag = true;
+			
+			String id = UUID.randomUUID().toString();
+			joinInfo.put("id", id);
+			sql.insert(NAMESPACE + "insertNewUser", joinInfo);
+		}
+		
+		result.put("username", Util.mapToString(joinInfo, "username"));
+		result.put("joinFlag", joinFlag);
+		
+		
+		return result;
 	}
 }
